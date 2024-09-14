75758cb51c584f01484c6f70a36d149355475c35
Vishallyours <vishusain0786@gmail.com>
2024-09-14 23:24:37 +0530

    socketurl update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mern-chat-app-rgq1.onrender.com", {
+			const socket = io("https://vchat-itq9.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
